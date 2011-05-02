66c6ba845b4d5e91f6652862dc4353f4fab9ec51
Xavier Glorot <glorotxa@iro.umontreal.ca>
2011-05-02 13:47:20 -0400

    new creation

--- phase1-text-modality-NetScale/src/CreateRep.py
+++ phase1-text-modality-NetScale/src/CreateRep.py
@@ -8,7 +8,6 @@ from exp_scripts.DARPAscript import createvecfile
         
 def CreateRep(ModelPath,DataPrefix):
     createvecfile(ModelPath,DataPrefix+ '.vec',3,DataPrefix+ '_DL3.vec')
-    createvecfile(ModelPath,DataPrefix+ '.vec',1,DataPrefix + '_DL1.vec')
 
 
 if __name__ == '__main__':
